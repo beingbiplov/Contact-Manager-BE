fe29dcf269f0b90e031059aa2790bb2c98ab50cb
beingbiplov <sharmabiplov@gmail.com>
2022-08-17 16:26:47 +0545

    add production config to knexfile

--- src/db/knexfile.ts
+++ src/db/knexfile.ts
@@ -21,6 +21,23 @@ const config: { [key: string]: Knex.Config } = {
       tableName: "knex_migrations",
     },
   },
+  production: {
+    client: process.env.DB_CLIENT,
+    connection: {
+      database: process.env.DB_NAME,
+      user: process.env.DB_USER,
+      host: process.env.DB_HOST,
+      password: process.env.DB_PASSWORD,
+      ssl: { rejectUnauthorized: false },
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+  },
 };
 
 export default config;
