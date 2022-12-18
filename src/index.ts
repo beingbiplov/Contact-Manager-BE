031a9774f580ff12b9429da73bb11efb846a5235
Biplov <sharmabiplov@gmail.com>
2022-12-18 10:32:22 +0545

    Add swagger documentation

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,24 @@ import cors from "cors";
 import dotenv from "dotenv";
 import express, { Request, Response, Application } from "express";
 
-import { notFound } from "./middlewares/notFound";
-import { errorHandler } from "./middlewares/errorHandler";
 import logger from "./misc/logger";
 import appRoutes from "./routes/index";
+import { notFound } from "./middlewares/notFound";
+import { errorHandler } from "./middlewares/errorHandler";
+
+const path = require("path");
+const swagger_path = path.resolve(__dirname, "./swagger.yaml");
+
+const swaggerUi = require('swagger-ui-express')
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load(swagger_path);
 
 dotenv.config();
 
 const app: Application = express();
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb" }));
